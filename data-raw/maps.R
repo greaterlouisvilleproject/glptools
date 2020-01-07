c75d76c71e982bb94bc9fe2263f2218957d59725
Harrison Kirby <harrisonk.kirby@gmail.com>
2020-01-07 15:33:46 -0500

    Added Metro United Way map

--- data-raw/maps.R
+++ data-raw/maps.R
@@ -2,10 +2,10 @@ library(dplyr)
 library(readr)
 library(magrittr)
 library(rgdal)
-library(glptools)
 
 path <- "data-raw/maps/"
 
+# Crosswalks
 
 nh_tract <- read_csv(path %p% "crosswalks/tract_to_nh.csv", col_types = "cc")
 attr(nh_tract, 'spec') <- NULL
@@ -13,12 +13,22 @@ attr(nh_tract, 'spec') <- NULL
 ma_tract <- read_csv(path %p% "crosswalks/tract_to_ma.csv", col_types = "cc")
 attr(ma_tract, 'spec') <- NULL
 
-ma_tract %<>%
-  rename(market_area = `Market Area`)
+ma_tract %<>% rename(market_area = `Market Area`)
 
 watterson_tract <- read_csv(path %p% "crosswalks/tract_to_watterson.csv", col_types = "cn")
 attr(watterson_tract, 'spec') <- NULL
 
+muw_tract <- read_csv(path %p% "MUW/Jefferson_Tract_Neighborhood.csv",
+                      skip = 2, col_names = TRUE, col_types = "cc")
+attr(muw_tract, 'spec') <- NULL
+
+muw_tract %<>%
+  transmute(
+    GEO_ID = "1400000US21111" %p% `Tract FIPS Code`,
+    neighborhood = Neighborhood)
+
+
+#Shapefiles
 
 map_block_group <- readOGR(path %p% "block_group", layer = "tl_2018_21_bg",
                            GDAL1_integer64_policy = TRUE, stringsAsFactors = FALSE, verbose = FALSE)
@@ -32,8 +42,10 @@ map_tract_2000 <- readOGR(path %p% "tract2000", layer = "tr21_d00",
 map_nh <- readOGR(path %p% "neighborhood", layer = "neighborhood",
                   GDAL1_integer64_policy = TRUE, stringsAsFactors = FALSE, verbose = FALSE)
 
+suppressWarnings(
 map_zip <- readOGR(path %p% "zip", layer = "Jefferson_County_KY_ZIP_Codes",
                    GDAL1_integer64_policy = TRUE, stringsAsFactors = FALSE, verbose = FALSE)
+)
 
 map_market <- readOGR(path %p% "market area", layer = "market area",
                       GDAL1_integer64_policy = TRUE, stringsAsFactors = FALSE, verbose = FALSE)
@@ -41,54 +53,62 @@ map_market <- readOGR(path %p% "market area", layer = "market area",
 map_county <- readOGR(path %p% "county", layer = "cb_2017_us_county_5m",
                       GDAL1_integer64_policy = TRUE, stringsAsFactors = FALSE, verbose = FALSE)
 
+# Create MUW map
+map_muw <- map_tract
+
+map_muw@data %<>% left_join(muw_tract, by = "GEO_ID")
+
+row.names(map_muw) <- row.names(map_muw@data)
+map_muw <- spChFIDs(map_muw, row.names(map_muw))
+
+map_muw <- rgeos::gUnaryUnion(map_muw, id = map_muw@data$neighborhood)
+
+nh_names <- row.names(map_muw)
+
+nh_names <- data.frame(neighborhood = nh_names, row.names = nh_names, stringsAsFactors = FALSE)
+
+map_muw <- SpatialPolygonsDataFrame(map_muw, nh_names)
+
 
 map_block_group <- map_block_group[map_block_group@data$COUNTYFP == "111",]
 map_block_group@data %<>%
   transmute(
     GEO_ID = GEOID,
     tract = TRACTCE,
-    block_group = as.numeric(BLKGRPCE),
-    line1 = paste0("Block group ", block_group))
+    block_group = as.numeric(BLKGRPCE))
 
 map_tract@data %<>%
   left_join(nh_tract, by = "GEO_ID") %>%
   transmute(
     GEO_ID,
     tract = as.numeric(NAME),
-    neighborhood,
-    line1 = paste0("Tract ", tract, " in the "),
-    line2 = paste0(neighborhood, " neighborhood"))
+    neighborhood)
 
 map_tract_2000 <- map_tract_2000[map_tract_2000@data$COUNTY == "111",]
 map_tract_2000@data %<>%
   transmute(
-    tract = NAME,
-    line1 = paste0("Tract ", as.numeric(tract)))
+    tract = NAME)
 
 map_zip@data %<>%
   transmute(
-    zip = as.numeric(ZIPCODE),
-    line1 = paste0("Zip code ", zip))
+    zip = as.numeric(ZIPCODE))
 
 map_nh@data %<>%
   transmute(
-    neighborhood = Neighborho,
-    line1 = neighborhood)
+    neighborhood = Neighborho)
 
 map_market@data %<>%
   transmute(
-    market = Area,
-    line1 = market)
+    market = Area)
 
 map_county@data %<>%
   transmute(
     FIPS = FIPS,
-    county = NAME,
-    line1 = county)
+    county = NAME)
 
-usethis::use_data(nh_tract, ma_tract, watterson_tract,
+usethis::use_data(nh_tract, ma_tract, watterson_tract, muw_tract,
                   map_block_group, map_tract_2000, map_zip, map_market, map_county,
                   overwrite = TRUE)
 
 # Publicly visible versions are written by glpdata
-usethis::use_data(map_tract, map_nh, overwrite = TRUE, internal = TRUE)
+usethis::use_data(map_tract, map_nh, map_muw, overwrite = TRUE, internal = TRUE)
