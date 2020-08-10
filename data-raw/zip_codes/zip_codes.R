28818bc92b6990b46b601d1dac3ee18542b2e049
Harrison Kirby <harrisonk.kirby@gmail.com>
2020-08-10 15:02:26 -0400

    Updated zipcode crosswalk
    Include 0 population zip codes, add in business apportionment

--- data-raw/zip_codes/zip_codes.R
+++ data-raw/zip_codes/zip_codes.R
@@ -2,47 +2,37 @@ library(readr)
 library(dplyr)
 library(magrittr)
 library(tidyr)
-source("R/general.R")
 
 path <- "data-raw/zip_codes/"
 
-zip_pop <- read_csv(path %p% "ACS_17_5YR_B01003_with_ann.csv", skip = 1, col_types = "cccd_")
 zip_crosswalk <- read_csv(path %p% "ZIP_COUNTY_122017.csv", col_types = "ccdddd")
 
-zip_pop %<>%
-  transmute(
-    zip = Id2,
-    population = `Estimate; Total`)
-
 zip_crosswalk %<>%
-  left_join(zip_pop, by = "zip") %>%
   transmute(
     FIPS = county,
     zip,
-    population_total = population %>% replace_na(0),
-    population_in_FIPS = population_total * res_ratio %>% replace_na(0),
-    pct_in_county = res_ratio * 100)
+    pct_pop_in_county = res_ratio * 100,
+    business_in_county = bus_ratio * 100,
+    total_in_county = tot_ratio * 100)
 
 MSA_zip <- zip_crosswalk %>%
   pull_peers(geog = "MSA", add_info = F) %>%
   left_join(MSA_FIPS, by = "FIPS") %>%
   group_by(zip, MSA) %>%
   summarise(
-    population_total = mean(population_total),
-    population_in_MSA = sum(population_in_FIPS),
-    pct_in_MSA = sum(pct_in_county)) %>%
-  ungroup() %>%
-  left_join(MSA_df, by = "MSA") %>%
-  select(MSA, zip, population_total, population_in_MSA)
+    pct_in_MSA = sum(pct_pop_in_county),
+    business_in_county = sum(business_in_county),
+    total_in_county = sum(total_in_county),
+    .groups = "drop") %>%
+  select(MSA, zip, pct_in_MSA, business_in_county, total_in_county)
 
 FIPS_zip <- zip_crosswalk %>%
   pull_peers(add_info = F) %>%
-  select(FIPS, zip, population_total, population_in_FIPS)
+  select(FIPS, zip, pct_pop_in_county, business_in_county, total_in_county)
 
 FIPS_zip_full_MSA <- zip_crosswalk %>%
   pull_peers(geog = "MSA", add_info = F) %>%
-  select(FIPS, zip, population_total, population_in_FIPS)
+  select(FIPS, zip, pct_pop_in_county, business_in_county, total_in_county)
 
-usethis::use_data(FIPS_zip, overwrite = TRUE)
-usethis::use_data(MSA_zip, overwrite = TRUE)
+usethis::use_data(MSA_zip, FIPS_zip, overwrite = TRUE)
 usethis::use_data(FIPS_zip_full_MSA, overwrite = TRUE, internal = TRUE)
