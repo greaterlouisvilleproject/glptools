065c9eb778e08aedf4b014508ade303dbf73c695
Harrison Kirby <harrisonk.kirby@gmail.com>
2020-08-10 15:04:43 -0400

    Update get all data file

--- data-raw/generate_all_data.R
+++ data-raw/generate_all_data.R
@@ -5,19 +5,16 @@ source("data-raw/basic_info/basic_info.R")
 source("data-raw/MSA_FIPS/MSA_FIPS.R")
 
 # MSA and FIPS to PUMA crosswalks
-source("data-raw/MSA_PUMA.R")
-source("data-raw/FIPS_PUMA.R")
+source("data-raw/FIPS_PUMA/FIPS_PUMA.R")
 
 # MSA and FIPS to zip code crosswalks
-source("data-raw/zip_codes.R")
+source("data-raw/zip_codes/zip_codes.R")
+
 # Tract 2000 to tract 2010 crosswalks
-source("data-raw/tract_crosswalk.R")
+source("data-raw/tract_crosswalk/tract_crosswalk.R")
 
 # Inflation and Cost of Living adjustment data frame
 source("data-raw/inflation_and_COLA.R")
 
-# Population data frames
-source("data-raw/population.R")
-
 # Maps
-source("data-raw/maps.R")
+source("data-raw/maps/maps.R")
